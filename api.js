0324a786c8508320a2572eb1f74ee5f1dee409f9
Jay Krishna Reddy <jaykrishnareddy@gmail.com>
2021-04-10 23:12:32 +0530

    initial

--- api.js
+++ api.js
@@ -6,7 +6,28 @@ const app = express();
 
 app.listen(5000,()=>console.log("listening on 5000"));
 
-//routes
+//Swagger Configuration
+const swaggerOptions = {
+    swaggerDefinition :{
+        info:{
+            title:'Employee API',
+            version:'1.0.0'
+        }
+    },
+    apis:['api.js'],
+}
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs));
+ 
+/**
+ * @swagger
+ * /Employees:
+ * get:
+ * description: Get All Employee
+ * responses: 
+ * 200: 
+ * description:Success
+ */
 app.get('/Employees',(req,res)=>{
     res.send([
         {
